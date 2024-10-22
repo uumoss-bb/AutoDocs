12b6dd59e47f211e0a9f827205dba18e1c9fc040
TM_Brodie <brodie.balser@ticketmaster.com>
2024-10-22 09:39:54 -0400

    completed get data schema

--- frontend/cli/displayDataSchemas.spec.ts
+++ frontend/cli/displayDataSchemas.spec.ts
@@ -1,6 +1,14 @@
 import searchDataSchemas from "../../businessLogic/searchDataSchemas";
 import shell from 'shelljs'
+import { prettyJSON } from "../../shared/normalizers";
 
 it('Display Data Schema', () => {
   const dataSchemas = searchDataSchemas()
+  const fileNames = Object.keys(dataSchemas)
+  shell.echo(prettyJSON(fileNames))
+  for (const [fileName, schema] of Object.entries(dataSchemas)) {
+    shell.echo('---------------------------')
+    shell.echo(`\n\t${fileName}`)
+    shell.echo(`\n${schema}`)
+  }
 })
