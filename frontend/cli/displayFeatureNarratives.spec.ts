8f03dbe5fe61aed386823fc1e91e28755bfae32c
TM_Brodie <brodie.balser@ticketmaster.com>
2024-10-26 14:50:25 -0400

    we are now writeing data to a personal_storage file

--- frontend/cli/displayFeatureNarratives.spec.ts
+++ frontend/cli/displayFeatureNarratives.spec.ts
@@ -1,8 +1,10 @@
 import searchFeatureNarratives from '../../business_logic/searchFeatureNarratives'
 import shell from 'shelljs'
+import FileSystem from '../../backend/FileSystem'
 
 it("ALPHA PROTOCOL", () => {
   const narrativeDetails = searchFeatureNarratives()
+
   const narrativeDetailsArray = Object.keys(narrativeDetails)
   narrativeDetailsArray.forEach(fileName => {
     shell.echo(`\n\tThe story behind ${fileName}`)
@@ -15,4 +17,9 @@ it("ALPHA PROTOCOL", () => {
       }
     })
   })
+
+  const writeNarrativeFileResult = FileSystem.writePersonalFile('feature_narratives', narrativeDetails)
+  if(writeNarrativeFileResult.error) {
+    console.log("FAILED TO WRITE NARRATIVE FILE")
+  }
 })
---
+++ shared/enums/fileNames.ts
@@ -0,0 +1,2 @@
+
+export const PERSONAL_STORAGE = '.personal_storage'
